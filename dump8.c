ad8ec26232205058de2c29dceda47b5575a0c32d
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-08-12 07:15:58 -0700

    ada

--- dump8.c
+++ dump8.c
@@ -1,6 +1,7 @@
 #include "mec_mr3_dict.h"
 
 #include <assert.h>
+#include <iconv.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,10 +38,13 @@ static size_t stream_read(void *ptr, size_t size, size_t nmemb,
 
 struct app {
   struct stream *in;
+  iconv_t conv;
 };
 
 static struct app *create_app(struct app *self, struct stream *in) {
   self->in = in;
+  self->conv = iconv_open("utf-8", "shift-jis");
+  assert(self->conv != (iconv_t)-1);
 
   return self;
 }
@@ -128,16 +132,190 @@ static bool read_data(struct app *self, const uint8_t group,
   return true;
 }
 
+enum Type {
+  ISO_8859_1_STRING =
+      0x00000300,          // ASCII string / or struct with 'ISO-8859-1' marker
+  STRUCT_436 = 0x001f4300, // Fixed struct 436 bytes (struct with ASCII strings)
+  STRUCT_516 = 0x001f4400, // Fixed struct 516 bytes (struct with ASCII strings)
+  STRUCT_325 = 0x001f4600, // Fixed struct 325 bytes (struct with ASCII strings)
+  SHIFT_JIS_STRING = 0xff002c00, // SHIFT-JIS string
+};
+
+struct buffer19 {
+  char sig1[0x3];
+  unsigned char len2;
+  char sig2;
+  unsigned char len3;
+  char sig3;
+  char iso[0x9];
+  char sig4;
+  unsigned char len4;
+  char sig5;
+};
+
+static void dump2file(const char *in, int len) {
+  static int debug = 0;
+  char buffer[512];
+  sprintf(buffer, "out%04d", debug);
+  ++debug;
+  FILE *f = fopen(buffer, "wb");
+  fwrite(in, 1, len, f);
+  fclose(f);
+}
+
+static bool print_iso(void *ptr, size_t size, size_t nmemb, struct app *self) {
+  static const char magic[] = {0xdf, 0xff, 0x79};
+  if (nmemb >= sizeof magic && memcmp(ptr, magic, sizeof(magic)) == 0) {
+    // iso
+    struct buffer19 b19;
+    if (nmemb < sizeof b19)
+      return 0;
+    memcpy(&b19, ptr, sizeof b19);
+    if (b19.sig2 != 0x1 || b19.sig3 != 0x0 || b19.sig4 != 0x2 ||
+        b19.sig5 != 0x0)
+      return 0;
+    const size_t diff = nmemb - sizeof b19;
+    if (b19.len2 != nmemb - 4 || b19.len3 != 9 || b19.len4 != diff)
+      return 0;
+    if (strncmp(b19.iso, "ISO8859-1", 9) != 0)
+      return 0;
+    char *str = (char *)ptr + sizeof b19;
+    {
+      char *gbk_str = str;
+      char dest_str[100];
+      char *out = dest_str;
+      size_t inbytes = b19.len4;
+      size_t outbytes = sizeof dest_str;
+      if (iconv(self->conv, &gbk_str, &inbytes, &out, &outbytes) ==
+          (size_t)-1) {
+        dump2file(gbk_str, inbytes);
+        printf("{%.*s : %.*s}", 9, b19.iso, b19.len4, str);
+        fflush(stdout);
+        assert(0);
+      }
+      dest_str[sizeof dest_str - outbytes] = 0;
+      printf("{%.*s : %.*s}", 9, b19.iso, outbytes, dest_str);
+    }
+  } else {
+    // raw string buffer
+    printf("[%.*s]", (int)nmemb, (char *)ptr);
+  }
+  return true;
+}
+
+typedef char str16[16 + 1];
+typedef char str64[64 + 1];
+
+struct buffer436 {
+  uint32_t zero;
+  char iver[0x45];
+  char buf3[0x100]; // phi
+  str64 buf4;
+  str16 buf5;
+  char modality[0x15];
+  uint32_t val;
+};
+struct buffer325 {
+  str64 array[5];
+};
+
+void print_buffer436(struct buffer436 *b436) {
+  static const char vers1[] = "TM_MR_DCM_V1.0";
+  static const char vers2[] = "TM_MR_DCM_V2.0";
+  static const char vers3[] = "TM_MR_DCM_V1.0_3";
+  static const char vers4[] = "TM_MR1_DCM_V1.0";
+  assert(b436->zero == 0);
+  assert(strcmp(b436->iver, vers1) == 0 || strcmp(b436->iver, vers2) == 0 ||
+         strcmp(b436->iver, vers3) == 0 || strcmp(b436->iver, vers4) == 0);
+  assert(strcmp(b436->modality, "MR") == 0);
+  assert(b436->val == 1 || b436->val == 3);
+  printf("{%u; %sr; %s; %s; %s; %s;%u}", b436->zero, b436->iver, b436->buf3,
+         b436->buf4, b436->buf5, b436->modality, b436->val);
+}
+
+struct buffer516 {
+  str64 zero; // aka 'none'
+  char buf2[0x15];
+  char buf3[0x100]; // phi
+  str16 buf4;
+  str64 buf5;
+  str64 buf6;
+  uint32_t bools[6];
+};
+
+void print_buffer516(struct buffer516 *b516) {
+  printf("{%s; %s; %s; %s; %s; %s; (", b516->zero, b516->buf2, b516->buf3,
+         b516->buf4, b516->buf5, b516->buf6);
+  int c;
+  for (c = 0; c < 6; ++c) {
+    assert(b516->bools[c] == c % 2);
+    if (c)
+      printf(",");
+    printf("%d", b516->bools[c]);
+  }
+  printf(")}");
+}
+
+void print_buffer325(struct buffer325 *b325) {}
+
+static bool print_struct(void *ptr, size_t size, size_t nmemb,
+                         struct app *self) {
+  struct stream *instream = self->in;
+
+  assert(size == 1);
+  const size_t s = nmemb;
+  if (s == 436) {
+    struct buffer436 b436;
+    memcpy(&b436, ptr, nmemb);
+    print_buffer436(&b436);
+  } else if (s == 516) {
+    struct buffer516 b516;
+    memcpy(&b516, ptr, nmemb);
+    print_buffer516(&b516);
+  } else if (s == 325) {
+    struct buffer325 b325;
+    memcpy(&b325, ptr, nmemb);
+    print_buffer325(&b325);
+  } else {
+    assert(0); // programmer error
+    return 0;
+  }
+  return true;
+}
+
 static bool print(struct app *self, const uint8_t group,
                   const struct mec_mr3_info *info,
                   struct mec_mr3_item_data *data) {
+  const char *name = get_mec_mr3_info_name(group, info->key, info->type);
   const uint32_t sign = info->type >> 24;
   const char symb = sign ? '_' : ' ';
 
-  printf("(%01x,%05x) %c%04x", group, info->key, symb,
+  bool ret = true;
+  uint32_t mult = 1;
+  // print info
+  printf("(%01x,%05x) %c%04x ", group, info->key, symb,
          (info->type & 0x00ffff00) >> 8);
+  // print data:
+  switch (info->type) {
+  case ISO_8859_1_STRING:
+    ret = print_iso(data->buffer, 1, data->len, self);
+    break;
+  case STRUCT_436:
+  case STRUCT_516:
+  case STRUCT_325:
+    ret = print_struct(data->buffer, 1, data->len, self);
+    break;
+  case SHIFT_JIS_STRING:
+    // s = print_shift_jis(data->buffer, 1, data->len, self);
+    break;
+  default:
+    ret = true;
+  }
+  // print key name
+  printf(" # %u,%u %s", data->len, mult, name);
+
   printf("\n");
-  return true;
+  return ret;
 }
 
 static bool read_group(struct app *self, uint8_t group, uint32_t nitems,
@@ -204,6 +382,7 @@ static bool mec_mr3_print(const void *input, size_t len) {
   }
   // release memory:
   free(data.buffer);
+  iconv_close(self->conv);
   if (!good)
     return false;
 
--- mec_mr3_dict.h
+++ mec_mr3_dict.h
@@ -5,3 +5,4 @@
 
 void check_mec_mr3_dict();
 bool check_mec_mr3_info(uint8_t group, uint32_t key, uint32_t type);
+const char *get_mec_mr3_info_name(uint8_t group, uint32_t key, uint32_t type);
